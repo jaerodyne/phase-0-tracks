1b333f6a88429120057e6cb023be849452de4545
Jillian Somera <jsomera@gmail.com>
2016-04-19 19:41:27 -0500

    Update Ruby Pair Work

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,19 @@
+empty_variable = []
+
+p empty_variable
+
+empty_variable = ["spoon", "fork", "knife", "plate", "glass"]
+
+p empty_variable
+
+p empty_variable.delete_at(2)
+
+p empty_variable
+
+empty_variable.insert(2, "candy")
+
+p empty_variable
+
+empty_variable.first()
+
+
