c2c9742be724f0746faff1ad80900ab615589ce7
Jillian Somera <jsomera@gmail.com>
2016-04-20 14:42:35 -0500

    Add Array Methods

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,14 @@
+def array(x,y,z)
+  a = [x,y,z]
+  print a
+end
+
+def new_array(x,y,z,new_item)
+  a = [x, y, z]
+  a.push(new_item)
+  print a
+end
+
 empty_variable = []
 
 p empty_variable
@@ -14,6 +25,22 @@ empty_variable.insert(2, "candy")
 
 p empty_variable
 
-empty_variable.first()
+empty_variable.shift
 
+p empty_variable
 
+if empty_variable.include? ("glass")
+  puts "this array includes glass"
+end
+
+food = ["pizza", "donuts", "chinese food", "chicken wings", "mango lassi"]
+
+dinner = empty_variable + food
+
+p dinner
+
+array("a", "b", "c")
+
+new_array("a", "b", "c", "d")
+
+new_array("orange", "hunger", 3, 6, 8)
