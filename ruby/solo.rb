ada09efeae467310966db71d8474e610fa24bbda
Jillian Somera <jsomera@gmail.com>
2016-04-29 00:00:20 -0500

    Add call methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,5 +1,5 @@
 #create class for Guitar
-#define attributes: brand_name, model, color, cost
+#define attributes: brand_name, model, color
 #3 methods: play, switch pickups, adjust volume
 
 #use attr_reader and attr_accessor
@@ -8,23 +8,20 @@
 class Guitar
   attr_reader :brand_name 
   # :budget
-  attr_accessor :model
-  # attr_accessor :pickup :model :color :cost
+  attr_accessor :model, :color
+  # attr_accessor :pickup
 
   def initialize
     @brand_name = brand_name
     puts "#{brand_name}"
     @model = model
     puts "#{model}"
-    # @color = color
-    # puts "#{color}"
-    # @cost = cost
-    # puts "#{cost}"
-    # @guitar_collection = ['Hollowbody II', '513', 'Custom 24']
+    @color = color
+    puts "#{color}"
   end
 
-  def play(guitar)
-    puts "Ready to play 3 chords on this #{guitar}!"
+  def play(guitar, color)
+    puts "Ready to play 3 chords on this #{color} #{guitar}?!"
     puts "Just kidding, let's play something else to celebrate International Jazz Day."
   end
 
@@ -33,25 +30,25 @@ class Guitar
   end
 
   def volume(level)
-    "Let's turn that volume dial to #{level}."
-  # randomize volume
-  # create range of volume from 0 - 20
-  # if volume 0-5, turn down
-  # if volume 6-10, turn up
-  # do you really want to end up using a hearing aid?
+    puts "Let's turn that volume dial to #{level}."
+    if level.between?(0, 5)
+      puts "What's that? I can barely hear you."
+    elsif level.between?(6, 10)
+      puts "Turn down for what?"
+    else
+      puts "How much hearing loss do you have now?"
+    end
   end
 
 end
 
-# prs = Guitar.new('PRS', 'Hollowbody II', 'Blood Orange', '$4000')
-# prs.switch_pickups('bridge')
-# puts prs.model
-# puts prs.switch_pickups('middle')
-
-pickups = ['bridge', 'bridge and middle combo', 'middle', 'middle and neck combo', 'neck']
 #initialize guitar
+axes = []
 new_guitar = Guitar.new
+axes << new_guitar 
+puts axes
 #start user interface
+brands = []
 puts "Ready to play guitar? Let's grab a PRS. If you want to use another brand, type it in. Otherwise, type in PRS."
 brand_name = gets.chomp
 #force PRS answer
@@ -59,20 +56,71 @@ while brand_name != 'PRS'
   puts "You're kidding right? PRS is the way to go. Type in PRS."
   brand_name = gets.chomp
 end
-
+brands << (brand_name)
+puts brands
 puts "Glad you came to your senses."
+
+#pick one of 3 in guitar_collection
+guitar_models = []
 puts "So what model do you want to use? (Hollowbody II/513/Custom 24)."
 guitar_collection = ['Hollowbody II', '513', 'Custom 24']
-
-#pick one of 3
 model = gets.chomp
 
 until guitar_collection.include? model
   puts "Looks like we don't have one in our collection yet. Choose the Hollowbody II/513/Custom 24."
   model = gets.chomp
 end
-new_guitar.play(model)
-# puts "Good choice."
-# puts "Now what color would you like?"
-# color = gets.chomp
-# puts "Sweet. Personally I prefer Blood Orange myself."
+
+guitar_models << (model)
+puts guitar_models
+
+#get color
+colors = []
+puts "Now what color would you like?"
+color = gets.chomp
+puts "Sweet. Personally, I prefer blood orange myself."
+
+colors << (color)
+puts colors
+
+#run play method in class
+new_guitar.play(model, color)
+
+pickups_collection = ['bridge', 'middle','neck']
+
+#pick pickups (ahem)
+chosen_pickup = []
+puts "What pickup should we use? (bridge/middle/neck)"
+pickup = gets.chomp
+
+until pickups_collection.include? pickup
+  puts "That's not an option. Pick again. (bridge/middle/neck)"
+  pickup = gets.chomp
+end
+
+chosen_pickup << pickup
+puts chosen_pickup
+new_guitar.switch_pickups(pickup)
+
+
+if pickup == "bridge"
+  puts "For jazz? Are you sure?"
+elsif pickup == "middle"
+  puts "We can work with that."
+else
+  puts "Yeah, that's more like it."
+end
+
+puts "Pump up the volume!"
+# create range of volume from 0 - 15
+volume_range = (0...15).to_a
+# randomize volume
+level = new_guitar.volume(volume_range.sample)
+
+# new_guitar do
+#   puts "Brand: #{brand_name}"
+#   puts "Model: #{model}"
+#   puts "Color: #{color}"
+#   puts "Pickup: #{pickup}"
+#   puts "Level: #{level}"
+# end
