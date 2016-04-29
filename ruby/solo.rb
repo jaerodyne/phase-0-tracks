4edf09161a5509b67c7b88c5851ceaf9742595ab
Jillian Somera <jsomera@gmail.com>
2016-04-29 16:03:55 -0500

    Add print statements for arrays

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -8,7 +8,7 @@
 class Guitar
   attr_reader :brand_name 
   # :budget
-  attr_accessor :model, :color
+  attr_accessor :model, :color, :pickup
   # attr_accessor :pickup
 
   def initialize
@@ -41,86 +41,88 @@ class Guitar
   end
 
 end
+ 
+affirmative = ""
 
-#initialize guitar
-axes = []
-new_guitar = Guitar.new
-axes << new_guitar 
-puts axes
-#start user interface
-brands = []
-puts "Ready to play guitar? Let's grab a PRS. If you want to use another brand, type it in. Otherwise, type in PRS."
+guitar_models = []
+colors = []
+chosen_pickup = []
+volume_levels = []
+
+puts "Let's grab a PRS. If you want to use another brand, type it in. Otherwise, type in PRS."
 brand_name = gets.chomp
 #force PRS answer
 while brand_name != 'PRS'
   puts "You're kidding right? PRS is the way to go. Type in PRS."
   brand_name = gets.chomp
 end
-brands << (brand_name)
-puts brands
 puts "Glad you came to your senses."
 
-#pick one of 3 in guitar_collection
-guitar_models = []
-puts "So what model do you want to use? (Hollowbody II/513/Custom 24)."
-guitar_collection = ['Hollowbody II', '513', 'Custom 24']
-model = gets.chomp
+until affirmative == "n"
 
-until guitar_collection.include? model
-  puts "Looks like we don't have one in our collection yet. Choose the Hollowbody II/513/Custom 24."
+  #initialize guitar
+  new_guitar = Guitar.new 
+  # axes << new_guitar
+   
+  #pick one of 3 in guitar_collection
+  puts "So what model do you want to use? (Hollowbody II/513/Custom 24)."
+  guitar_collection = ['Hollowbody II', '513', 'Custom 24']
   model = gets.chomp
-end
 
-guitar_models << (model)
-puts guitar_models
+  until guitar_collection.include? model
+    puts "Looks like we don't have one in our collection yet. Choose the Hollowbody II/513/Custom 24."
+    model = gets.chomp
+  end
+  
+  guitar_models << model
 
-#get color
-colors = []
-puts "Now what color would you like?"
-color = gets.chomp
-puts "Sweet. Personally, I prefer blood orange myself."
-
-colors << (color)
-puts colors
-
-#run play method in class
-new_guitar.play(model, color)
-
-pickups_collection = ['bridge', 'middle','neck']
-
-#pick pickups (ahem)
-chosen_pickup = []
-puts "What pickup should we use? (bridge/middle/neck)"
-pickup = gets.chomp
-
-until pickups_collection.include? pickup
-  puts "That's not an option. Pick again. (bridge/middle/neck)"
+  #get color
+  
+  puts "Now what color would you like?"
+  color = gets.chomp
+  puts "Sweet. Personally, I prefer blood orange myself."
+  
+  colors << color
+  
+  #run play method in class
+  new_guitar.play(model, color)
+  
+  pickups_collection = ['bridge', 'middle','neck']
+  
+  #pick pickups (ahem)
+  
+  puts "What pickup should we use? (bridge/middle/neck)"
   pickup = gets.chomp
+  
+  until pickups_collection.include? pickup
+    puts "That's not an option. Pick again. (bridge/middle/neck)"
+    pickup = gets.chomp
+  end
+  
+  chosen_pickup << pickup
+  new_guitar.switch_pickups(pickup)
+  
+  if pickup == "bridge"
+    puts "For jazz? Are you sure?"
+  elsif pickup == "middle"
+    puts "We can work with that."
+  else
+    puts "Yeah, that's more like it."
+  end
+  
+  puts "Pump up the volume!"
+  # create range of volume from 0 - 15
+  volume_range = (0...15).to_a
+  # randomize volume
+  level = new_guitar.volume(volume_range.sample)
+  # initialize new guitar?
+  puts "Do you want to grab another guitar? (y/n)"
+  affirmative = gets.chomp.downcase
+
 end
 
-chosen_pickup << pickup
-puts chosen_pickup
-new_guitar.switch_pickups(pickup)
-
-
-if pickup == "bridge"
-  puts "For jazz? Are you sure?"
-elsif pickup == "middle"
-  puts "We can work with that."
-else
-  puts "Yeah, that's more like it."
-end
-
-puts "Pump up the volume!"
-# create range of volume from 0 - 15
-volume_range = (0...15).to_a
-# randomize volume
-level = new_guitar.volume(volume_range.sample)
-
-# new_guitar do
-#   puts "Brand: #{brand_name}"
-#   puts "Model: #{model}"
-#   puts "Color: #{color}"
-#   puts "Pickup: #{pickup}"
-#   puts "Level: #{level}"
-# end
+puts "Here's your PRS collection!"
+puts "Model: #{guitar_models}"
+puts "Color: #{colors}"
+puts "Pickup: #{chosen_pickup}"
+ 
