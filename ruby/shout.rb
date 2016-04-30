63c44dcf82114a5e77e37cc605930d39590afc2f
Jillian Somera <jsomera@gmail.com>
2016-04-29 19:09:33 -0500

    Add shout moduleand 2 classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,39 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(celebrate)
+#     "#{celebrate.upcase} I just finished Week 6 of DBC!"
+#   end
+# end
+
+# Shout.yell_angrily "whoa"
+# Shout.yelling_happily "omg whaaaaat"
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+
+  def yell_angrily(words)
+    puts words + "!!!" + " :("
   end
 
-  def self.yelling_happily(celebrate)
-    "#{celebrate.upcase} I just finished Week 6 of DBC!"
+  def yelling_happily(celebrate)
+    puts "#{celebrate.upcase} I just finished Week 6 of DBC!"
   end
+
 end
 
-Shout.yell_angrily "whoa"
-Shout.yelling_happily "omg whaaaaat"
+class Elvis
+  include Shout
+end
+
+class Voters
+  include Shout
+end
+
+dance = Elvis.new
+dance.yelling_happily("You know you make me wanna shout!")
+
+disgruntled = Voters.new
+disgruntled.yell_angrily("THIS ELECTION THO")
+
