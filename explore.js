4ab71fd9523426184e3ed04379717a25f0a8e754
Jillian Somera <jsomera@gmail.com>
2016-05-02 15:32:17 -0500

    Add incrementing loop

---
+++ explore.js
@@ -0,0 +1,10 @@
+// For each letter in a given string, print starting from the last letter and go backwards until whole string is reversed.
+
+// Example: headache == ehcadaeh
+
+var str = "headache";
+var reversed = ""
+
+for (var i = 0; i = str.length, i++) {
+  console.log(str[i])
+}
