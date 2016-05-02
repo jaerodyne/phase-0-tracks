96a67cd1502c596d043a09a8ca9c2d83fce826e8
Jillian Somera <jsomera@gmail.com>
2016-05-02 16:25:26 -0500

    Add reverse string method

--- explore.js
+++ explore.js
@@ -1,10 +1,17 @@
 // For each letter in a given string, print starting from the last letter and go backwards until whole string is reversed.
+// Store result in variable
 
 // Example: headache == ehcadaeh
 
-var str = "headache";
-var reversed = ""
+// var str = "headache";
+// var reversed_string = " ";
 
-for (var i = 0; i = str.length, i++) {
-  console.log(str[i])
+function reverse(str) {
+  var reversed_string = ""
+  for (var i = str.length; i >= 0; i--) {
+  reversed_string = str[i] + reversed_string;
+  }
+  return reversed_string;
 }
+
+console.log(reverse("headache"));
