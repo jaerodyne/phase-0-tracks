42b6e36ba77808a082416eea2b91ab54fd8aee1a
Jillian Somera <jsomera@gmail.com>
2016-05-03 17:50:11 -0500

    Add horseAndColor and Car constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,69 +1,30 @@
-// CLASSLIKE BEHAVIOR
+// RELEASE 0
+var colors = ["blue", "red", "yellow", "white"]
 
-// In addition to literal construction, which is
-// done by simply typing out the data structure ...
-var dog = {name: 'Spot', 'age': 3, isGoodDog: true};
+var names = ["bob", "kevin", "becky", "joe"]
 
-// ... we can write what's called a constructor function
-// that can be used to make lots of dogs.
-// We don't HAVE to write a constructor function,
-// but if we know we'll need a bunch of objects with
-// the same properties, such functions are handy.
-// Constructor functions have capitalized names.
-function Dog(name, age, isGoodDog) {
-  // In this context, 'this' refers to 
-  // the individual dog we're making
-  console.log("Our new dog:", this);
-  
-  // Therefore, this.name is sort of the Ruby
-  // equivalent of @name
-  this.name = name;
-  this.age = age;
-  this.isGoodDog = isGoodDog;
-  
-  // As for behavior, a function is a perfectly 
-  // valid property value ... whoa!
-  this.bark = function() { console.log("Woof!"); };
-  //Like in Ruby class Syntax: @bark = bark
-  console.log("DOG INITIALIZATION COMPLETE");
+colors.push("cerulean")
+names.push("pablo")
+
+for (var i = -1; i < names.length -1; i++) {
+  horseAndColor[names[i+1]] = colors[i+1]
+}
+console.log(horseAndColor)
+
+function Car(brand, type, seats) {
+  this.brand = brand;
+  this.type = type;
+  this.seats = seats;
+  this.start = 
+    function()      
+    {console.log('Vroom vroom'); };
 }
 
-console.log("Let's build a dog ...");
-// Here is where we call that constructor function
-// that we just made ...
-var anotherDog = new Dog("Duchess", 8, true);
-console.log(anotherDog);
-console.log("Our dog can bark:");
-anotherDog.bark();
-console.log("----");
+var mycar = new Car('hyundai','suv', 4);
+mycar.start();
 
-console.log("Let's build another dog ...");
-var yetAnotherDog = new Dog("Fido", 4, false);
-console.log(yetAnotherDog.name);
-console.log("This new dog can bark, too:");
-yetAnotherDog.bark();
-console.log("----")
+var amandasCar = new Car('tesla', 'sedan', 4)
+amandasCar.start();
 
-// You can use functions as values anytime, not just
-// in constructor functions. Here we use literal
-// construction:
-var bird = {
-  name: 'Polly', 
-  'age': 3, 
-  wantsCracker: true, 
-    speak: function() { 
-      if (this.wantsCracker) {
-        console.log("Polly wants a cracker!");
-      } else {
-        console.log("Hello! Hello!");
-      }
-    }
-};
-
-bird.speak();
-bird.wantsCracker = false;
-bird.speak();
-
-// You can add behavior the way you'd add any other key-value pair.
-bird.newTrick = function() { console.log("*sings opera song*")};
-bird.newTrick();
+var jillsCar = new Car('mercedes', 'luxury', 4)
+jillsCar.start();
