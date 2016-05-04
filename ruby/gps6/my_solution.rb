7b804da7e682cae397f7fe8cbded3f57272db711
Jillian Somera <jsomera@gmail.com>
2016-05-03 19:36:17 -0500

    Add print_list method and refactor virus_effects method

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,8 +4,10 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# require_relative used relative paths to access files
+# require uses absolute path to access a file, so if the files are moved to another
+# directory, they won't be available in the file
+
 require_relative 'state_data'
 
 class VirusPredictor
@@ -17,13 +19,13 @@ class VirusPredictor
   end
 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +43,7 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -93,3 +95,16 @@ print_list
 
 #=======================================================================
 # Reflection Section
+
+# In the state_data file, the hash for the states is the key, while all of the population
+# data is used as the key. In the nested hash which contains both population and population density, 
+# which each serves as a key, followed by their respective values.
+
+# require_relative essentially links to the indicated file and allows all of that code
+# to be accessible within the file
+
+# You can iterate through a hash using a do loop. You can also use .map and .each
+
+# In refactoring virus_effects, I noticed that the method didn't take any arguments, so it wasn't necessary to define them within the method
+
+# I think I now understand how to create code that is DRY and how to iterate over a hash
