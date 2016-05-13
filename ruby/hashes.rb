1bccf139d2c54f99f46257fc2c38d7e3ed582a88
Jillian Somera <jsomera@gmail.com>
2016-05-13 13:11:33 -0500

    Revise pseudocode

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,44 +1,14 @@
-#Get user's name and convert to string
-puts "Enter in your name:"
-name = gets.chomp.to_s
-#Get address and convert to string
-puts "Address:"
-address = gets.chomp.to_s
-#Get email and convert to string
-puts "Email:"
-email = gets.chomp.to_s
-#Get phone number and convert to string
-puts "Phone Number:"
-phone = gets.chomp.to_s
-#Get number of children and convert to integer
-puts "Number of Children:"
-number_of_children = gets.chomp.to_i
-#Get decor theme and convert to string
-puts "What's your decor theme?"
-decor_theme = gets.chomp.to_s
-#Ask if user wants a game room
-puts "Do you want a game room? (yes/no)"
-game_room = gets.chomp.to_s
-#Ask if user wants a pool
-puts "Do you want a pool? (yes/no)"
-pool = gets.chomp.to_s
-#Ask if user wants carpet
-puts "Do you want carpet? (yes/no)"
-carpet = gets.chomp.to_s
-#Ask if user wants tiles
-puts "Do you want tiles? (yes/no)"
-tiles = gets.chomp.to_s
-#Ask if user wants wooden floors
-puts "Do you want wooden floors? (yes/no)"
-wooden_floors = gets.chomp.to_s
+#Create hash to store applicant info and set each key to user input's value
+#Create user interface to get input values
+#Print applicant hash to display user inputs
+#Ask user if they want to update any of the fields
 
-#Create hashes to store applicant info and set each key to user input's value
 applicant = {
-  name: name, 
+  name: name,
   address: address,
   email: email, 
   phone: phone,
-  number_of_children: number_of_children,
+  number_of_rooms: number_of_rooms,
   decor_theme: decor_theme,
   game_room: game_room,
   pool: pool,
@@ -47,10 +17,42 @@ applicant = {
   wooden_floors: wooden_floors
 }
 
-#Print applicant hash to display user inputs
+#USER INTERFACE
+puts "Enter in your name:"
+name = gets.chomp.to_s
+
+puts "Address:"
+address = gets.chomp.to_s
+
+puts "Email:"
+email = gets.chomp.to_s
+
+puts "Phone Number:"
+phone = gets.chomp.to_s
+
+puts "Number of rooms to be renovated:"
+number_of_rooms = gets.chomp.to_i
+
+puts "What's your decor theme?"
+decor_theme = gets.chomp.to_s
+
+puts "Do you want a game room? (yes/no)"
+game_room = gets.chomp.to_s
+
+puts "Do you want a pool? (yes/no)"
+pool = gets.chomp.to_s
+
+puts "Do you want carpet? (yes/no)"
+carpet = gets.chomp.to_s
+puts "Do you want tiles? (yes/no)"
+tiles = gets.chomp.to_s
+puts "Do you want wooden floors? (yes/no)"
+wooden_floors = gets.chomp.to_s
+
+
 puts applicant
 
-#Ask user whether or not to update decor theme
+
 puts "Are you sure about your decor theme? You'll have to live with it forever! (Type 'none' if you don't want to make any changes. Otherwise, press enter to move on.)"
 #Store user answer in affirmative variable
 affirmative = gets.chomp.downcase.to_s
