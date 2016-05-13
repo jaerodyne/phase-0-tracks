edaa7657c097510effc8d196ae153ad65aaccfd0
Jillian Somera <jsomera@gmail.com>
2016-05-13 13:45:59 -0500

    Add game_room conditionals

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,74 +2,77 @@
 #Create user interface to get input values
 #Print applicant hash to display user inputs
 #Ask user if they want to update any of the fields
-
-applicant = {
-  name: name,
-  address: address,
-  email: email, 
-  phone: phone,
-  number_of_rooms: number_of_rooms,
-  decor_theme: decor_theme,
-  game_room: game_room,
-  pool: pool,
-  carpet: carpet,
-  tiles: tiles,
-  wooden_floors: wooden_floors
-}
+#Create conditional statement to replace any value in applicant hash
+#Print final applicant hash
 
 #USER INTERFACE
 puts "Enter in your name:"
 name = gets.chomp.to_s
 
-puts "Address:"
-address = gets.chomp.to_s
+# puts "Address:"
+# address = gets.chomp.to_s
 
-puts "Email:"
-email = gets.chomp.to_s
+# puts "Email:"
+# email = gets.chomp.to_s
 
-puts "Phone Number:"
-phone = gets.chomp.to_s
+# puts "Phone Number:"
+# phone = gets.chomp.to_s
 
-puts "Number of rooms to be renovated:"
-number_of_rooms = gets.chomp.to_i
+# puts "Number of rooms to be renovated:"
+# number_of_rooms = gets.chomp.to_i
 
-puts "What's your decor theme?"
-decor_theme = gets.chomp.to_s
+# puts "What's your decor theme?"
+# decor_theme = gets.chomp.to_s
 
-puts "Do you want a game room? (yes/no)"
+puts "Do you want a game room? (y/n)"
 game_room = gets.chomp.to_s
+until game_room == "y" || game_room == "n"
+  puts "Please enter a valid answer. (y/n)"
+  game_room = gets.chomp.to_s
+end
 
-puts "Do you want a pool? (yes/no)"
-pool = gets.chomp.to_s
+# puts "Do you want a pool? (yes/no)"
+# pool = gets.chomp.to_s
 
-puts "Do you want carpet? (yes/no)"
-carpet = gets.chomp.to_s
-puts "Do you want tiles? (yes/no)"
-tiles = gets.chomp.to_s
-puts "Do you want wooden floors? (yes/no)"
-wooden_floors = gets.chomp.to_s
+# puts "Do you want carpet? (yes/no)"
+# carpet = gets.chomp.to_s
+# puts "Do you want tiles? (yes/no)"
+# tiles = gets.chomp.to_s
+# puts "Do you want wooden floors? (yes/no)"
+# wooden_floors = gets.chomp.to_s
 
+# puts applicant
 
-puts applicant
+# puts "Are you sure about your decor theme? You'll have to live with it forever! (Type 'none' if you don't want to make any changes. Otherwise, press enter to move on.)"
+# affirmative = gets.chomp.downcase.to_s
 
+# if affirmative == "none"
+#   puts "Well, if you're sure."
+# else
+#   applicant.delete(:decor_theme)
+#   puts "What decor theme would you like to use instead?"
+#   decor_theme_alt = gets.chomp
+#   applicant[:decor_theme] = decor_theme_alt
+#   puts "#{applicant[:decor_theme]} it is!"
+#   puts applicant
+# end
 
-puts "Are you sure about your decor theme? You'll have to live with it forever! (Type 'none' if you don't want to make any changes. Otherwise, press enter to move on.)"
-#Store user answer in affirmative variable
-affirmative = gets.chomp.downcase.to_s
+applicant = {
+  name: name,
+  # address: address,
+  # email: email, 
+  # phone: phone,
+  # number_of_rooms: number_of_rooms,
+  # decor_theme: decor_theme,
+  game_room: game_room
+  # pool: pool,
+  # carpet: carpet,
+  # tiles: tiles,
+  # wooden_floors: wooden_floors
+}
 
-#Create conditional statement to replace decor_theme in applicant hash
-if affirmative == "none"
-  puts "Well, if you're sure."
+if game_room == "y"
+  applicant[:game_room] = true
 else
-#Access decor_theme key in applicant hash and delete its value
-  applicant.delete(:decor_theme)
-#Ask user for new decor theme and store in decor_theme_alt variable
-  puts "What decor theme would you like to use instead?"
-  decor_theme_alt = gets.chomp
-#Set decor_theme hash value to new decor_theme_alt variable
-  applicant[:decor_theme] = decor_theme_alt
-#Print newly-generated decor_theme variable
-  puts "#{applicant[:decor_theme]} it is!"
-#Print updated applicant hash
-  puts applicant
+  applicant[:game_room] = false
 end
