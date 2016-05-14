12577edfa0514f6b97f8ceadb5d82359747b1ccc
Jillian Somera <jsomera@gmail.com>
2016-05-14 16:55:48 -0500

    Add pseudocode

---
+++ ruby/alias_manager2.rb
@@ -0,0 +1,5 @@
+#Get spy's real name
+#Swap first and last name
+#Change each vowel in name to next vowel {ex: a => e, e => i, etc.}
+#Change each consonant in name to next consonant {ex: b => c, c => d, etc.}
+#Ex: Felicia Torres => Vussit Gimodoe
