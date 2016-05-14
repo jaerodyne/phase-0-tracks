a8ba638825fb5cdff2e49103fe46498ee013ce98
Jillian Somera <jsomera@gmail.com>
2016-05-14 17:36:26 -0500

    Add next_letter method

--- ruby/alias_manager2.rb
+++ ruby/alias_manager2.rb
@@ -6,9 +6,45 @@
 
 #Subtract vowels from alphabet?
 
-def next_vowel(str)
-  str.gsub!(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
+# def next_vowel(str)
+#   str.gsub!(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
+#   puts str
+# end
+
+# next_vowel('aeiou')
+
+def next_letter(str)
+  str = str.downcase
+  str.gsub!(/[abcdefghijklmnopqrstvwxyz]/,
+  'a' => 'e', 
+  'b' => 'c',
+  'c' => 'd',
+  'd' => 'f',
+  'e' => 'i',
+  'f' => 'g',
+  'g' => 'h',
+  'h' => 'j',
+  'i' => 'o', 
+  'j' => 'k',
+  'k' => 'l',
+  'l' => 'm',
+  'm' => 'n',
+  'n' => 'p',
+  'o' => 'u',
+  'p' => 'q',
+  'q' => 'r',
+  'r' => 's',
+  's' => 't',
+  't' => 'v',
+  'u' => 'a',
+  'v' => 'w',
+  'w' => 'x',
+  'x' => 'y',
+  'y' => 'z',
+  'z' => 'b'
+  )
   puts str
 end
 
-next_vowel('aeiou')
+next_letter('abacus')
+
