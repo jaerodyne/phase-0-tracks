ec6cd7a687bc1a4463e82bd5dacc86ba5c94be25
Jillian Somera <jsomera@gmail.com>
2016-05-14 17:14:04 -0500

    Add next_vowel method

--- ruby/alias_manager2.rb
+++ ruby/alias_manager2.rb
@@ -3,3 +3,12 @@
 #Change each vowel in name to next vowel {ex: a => e, e => i, etc.}
 #Change each consonant in name to next consonant {ex: b => c, c => d, etc.}
 #Ex: Felicia Torres => Vussit Gimodoe
+
+#Subtract vowels from alphabet?
+
+def next_vowel(str)
+  str.gsub!(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
+  puts str
+end
+
+next_vowel('aeiou')
