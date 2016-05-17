50cb58f9a7bab62c076a4b37a7656a70a8b44c76
Jillian Somera <jsomera@gmail.com>
2016-05-17 12:22:27 -0500

    Add dog_years method

--- ruby/puppy_methods2.rb
+++ ruby/puppy_methods2.rb
@@ -14,6 +14,15 @@ class Puppy
     print "*rolls over*"
   end
 
+  def dog_years(int)
+    first_two_years = 2 * 10.5
+    int -= 2
+    years_after = int * 4
+    age = first_two_years + years_after
+    puts "Age: #{age}"
+    # For more details on dog year calculation: http://www.calculatorcat.com/dogs/dog-years.phtml
+  end
+
 end
 
 fido = Puppy.new
@@ -21,3 +30,4 @@ fido = Puppy.new
 fido.fetch("Fido")
 fido.speak(5)
 fido.roll_over
+fido.dog_years(3)
