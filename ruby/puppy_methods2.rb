dc5cb578f7ce53bd7c416cf8aaff2cf08f4b69f9
Jillian Somera <jsomera@gmail.com>
2016-05-17 11:23:49 -0500

    Add speak method

--- ruby/puppy_methods2.rb
+++ ruby/puppy_methods2.rb
@@ -4,8 +4,15 @@ class Puppy
     puts "#{name} fetched a ball!"
   end
 
+  def speak(int)
+    int.times do
+    puts "Woof!"
+    end
+  end
+
 end
 
 fido = Puppy.new
 
 fido.fetch("Fido")
+fido.speak(5)
