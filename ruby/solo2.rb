89b1e5bca6062cf31b91a59843a831b939c18946
Jillian Somera <jsomera@gmail.com>
2016-05-19 16:05:49 -0500

    Add donate method

--- ruby/solo2.rb
+++ ruby/solo2.rb
@@ -18,10 +18,14 @@ class Outfit
     puts "Time for retail therapy."
   end
 
-  
+  def donate(clothing)
+    puts "Let's put this #{clothing} to rest at Goodwill."
+  end
 
 end
 
 casual = Outfit.new('shirt', 'jeans', 'nikes', 'oakleys')
 puts "Lookin' so fly in your #{casual.top}, #{casual.bottom}, #{casual.shoes}, and #{casual.accessories}."
 casual.strut
+casual.go_shopping
+casual.donate('ragged toga')
