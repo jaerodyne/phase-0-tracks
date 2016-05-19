ba51f37264b07a21652e36340b5e639923e0e2e2
Jillian Somera <jsomera@gmail.com>
2016-05-19 15:57:30 -0500

    Add Outfit attributes

--- ruby/solo2.rb
+++ ruby/solo2.rb
@@ -1,2 +1,15 @@
 class Outfit
+
+  attr_accessor :top, :bottom, :shoes, :accessories
+
+  def initialize(top, bottom, shoes, accessories)
+    @top = top
+    @bottom = bottom
+    @shoes = shoes
+    @accessories = accessories
+  end
+
 end
+
+casual = Outfit.new('shirt', 'jeans', 'nikes', 'oakleys')
+puts "Lookin' so fly in your #{casual.top}, #{casual.bottom}, #{casual.shoes}, and #{casual.accessories}."
