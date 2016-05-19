380817f33437ff58d9662ae4a39ef31f94f77399
Jillian Somera <jsomera@gmail.com>
2016-05-19 17:08:49 -0500

    Add wardrobe hash

--- ruby/solo2.rb
+++ ruby/solo2.rb
@@ -30,3 +30,12 @@ puts "Lookin' so fly in your #{casual.top}, #{casual.bottom}, #{casual.shoes}, a
 casual.strut
 casual.go_shopping
 casual.donate('ragged toga')
+
+wardrobe = {
+  tops: ['Star Wars graphic tee', 'blouse', 'dress shirt'],
+  bottoms: ['jeans', 'skirt', 'jeggings'],
+  shoes: ['Adidas high tops', 'Jimmy Choos', 'Converse'],
+  accessories: ['Tiffany bracelet', 'Swarovski necklace', 'Rolex']
+}
+
+puts wardrobe[:tops]
