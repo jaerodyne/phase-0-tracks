29764edb250db05b1b423f4a67aaad0d445c96fa
Jillian Somera <jsomera@gmail.com>
2016-05-20 15:58:21 -0500

    Create Shout mixin module

--- ruby/shout2.rb
+++ ruby/shout2.rb
@@ -1,16 +1,27 @@
+# module Shout
+  
+#   def self.yell_angrily(words)
+#     puts words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily
+#     puts "It's raining men hallelujah!"
+#   end
+
+# end
+
+# Shout.yell_angrily 'PARKOUR'
+# Shout.yelling_happily
+
 module Shout
   
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     puts words + "!!!" + " :("
   end
 
-  def self.yelling_happily
-    puts "It's raining men hallelujah!"
+  def yelling_happily
+    puts "SPORTS GO SPORTS!"
   end
 
 end
-
-Shout.yell_angrily 'PARKOUR'
-Shout.yelling_happily
-
 
