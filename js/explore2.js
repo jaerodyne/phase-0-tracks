1b0e01300121bf8c4bf57bdc85b4f2871e122832
Jillian Somera <jsomera@gmail.com>
2016-05-24 11:00:42 -0500

    Add pseudocode

---
+++ js/explore2.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string and reverses it
+// declare a variable to store reversed string
+// set count in for loop
+// while string length is greater than count iterate through string and print out each letter
+// add letter to string
+// decrement count
+// return reversed string
+// ex: "hello" -> "olleh"
+
