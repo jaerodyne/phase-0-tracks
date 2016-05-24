57db9b80beded5ca88f83b10215067728ccc2b7b
Jillian Somera <jsomera@gmail.com>
2016-05-24 11:01:36 -0500

    Add driver code

--- js/explore2.js
+++ js/explore2.js
@@ -7,3 +7,12 @@
 // return reversed string
 // ex: "hello" -> "olleh"
 
+function reverseString(str) {
+  var reversedString = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i]
+  }
+  return reversedString
+}
+
+console.log(reverseString("bollocks"));
