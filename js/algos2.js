7a4c491958b239c965628a8791743e53970651d0
Jillian Somera <jsomera@gmail.com>
2016-05-25 08:24:27 -0500

    Add pseudocode

---
+++ js/algos2.js
@@ -0,0 +1,5 @@
+// Write a function that takes an array of words and returns the longest word or phrase in the array
+// Iterate through array and count the length of each word
+// Compare the length of each word
+// If word count is longest in the array, return the word
+
