3833211401f6e4d82652db7b3d315598fdf483ac
Jillian Somera <jsomera@gmail.com>
2016-05-28 00:20:36 -0500

    Declare longestWord function

--- js/algos2.js
+++ js/algos2.js
@@ -5,3 +5,17 @@
 // iterate through array and count the length of each word
 // if length of current word is shorter than the next word, next word becomes the current word and current word length is logged in count
 // else, break loop
+
+function longestWord(array) {
+	var word = ''
+	var count = 0
+		for (i = 0; i < array.length; i++ ) {
+			if (array[i].length > count) {
+				word = array[i];
+				count = word.length;
+			} else {
+				break;
+			}
+		}
+	return console.log('And the longest word is ' + word + '!')
+}
