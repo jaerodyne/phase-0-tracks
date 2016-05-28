afd58312e1f6fcab6b17f4e82b85f49a2071570f
Jillian Somera <jsomera@gmail.com>
2016-05-28 00:19:39 -0500

    Add pseudocode

--- js/algos2.js
+++ js/algos2.js
@@ -1,1 +1,7 @@
-
+// write a function that takes an array of words and returns the longest word
+// store current count in a variable
+// store current word in a variable
+// while i is less than the array length
+// iterate through array and count the length of each word
+// if length of current word is shorter than the next word, next word becomes the current word and current word length is logged in count
+// else, break loop
