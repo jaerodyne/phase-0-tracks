eb28b62120768e9ce40ec2017c2b4e6d8be87c4f
Jillian Somera <jsomera@gmail.com>
2016-06-06 03:09:54 -0500

    Add pseudocode

--- databases/meal_planner/meal_plan.rb
+++ databases/meal_planner/meal_plan.rb
@@ -1,1 +1,6 @@
+# Create a meal plan for a week
+# Generate meal for breakfast, lunch, dinner 7 times
+# Randomize each meal from collection of recipes
+# Create database that displays week's meal plan
+
 require 'sqlite3'
