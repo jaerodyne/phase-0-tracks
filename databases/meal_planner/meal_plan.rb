5964e34deb0242b03e259318bd9f78c9866dd050
Jillian Somera <jsomera@gmail.com>
2016-06-06 04:32:05 -0500

    Refactor meal_generator method

--- databases/meal_planner/meal_plan.rb
+++ databases/meal_planner/meal_plan.rb
@@ -11,26 +11,17 @@ db.results_as_hash = true
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS meals(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    type_id INT,
-    FOREIGN KEY (type_id) REFERENCES types(id)
-  );
-  CREATE TABLE IF NOT EXISTS types(
-    id INTEGER PRIMARY KEY,
     name VARCHAR(255)
-  );
 SQL
 
 db.execute(create_table_cmd)
-db.execute("INSERT INTO meals (name) VALUES ('Fruit Soup with Nuts')")
+# db.execute("INSERT INTO meals (name) VALUES ('Fruit Soup with Nuts')")
 
-def meal_generator(breakfast, lunch, dinner)
-  breakfast
-  lunch
-  dinner
+def meal_generator(food)
+  food
 end
 
 breakfast = ["Fruit Soup with Nuts", "Blueberry and Grilled Peach Quinoa Parfait", "Bacon and Eggs in a Jar"]
 lunch = ["Chicken Taco Salad", "Almost Instant Noodle Soup", "Mason Jar Pesto Pasta"]
 dinner = ["Mason Jar Lasagna", "Shepherd's Pie", "Cornbread Chili"]
-types = ["breakfast", "lunch", "dinner"]
+
