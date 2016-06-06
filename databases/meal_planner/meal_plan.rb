cc5b44d46b2f9ed6736a6e40cdd7dff755ab6912
Jillian Somera <jsomera@gmail.com>
2016-06-06 03:15:40 -0500

    Add SQL meal_plan table

--- databases/meal_planner/meal_plan.rb
+++ databases/meal_planner/meal_plan.rb
@@ -8,6 +8,15 @@ require 'sqlite3'
 db = SQLite3::Database.new("meal_plan.db")
 db.results_as_hash = true
 
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS meal_plan(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    type_id INT
+    FOREIGN KEY (type_id) REFERENCES types(id)
+  )
+SQL
+
 def meal_generator(breakfast, lunch, dinner)
   breakfast
   lunch
